05373c5ccc014972400b2633df89eba3b7024ba8
Nelson-JK <30672922+Nelson-JK@users.noreply.github.com>
2026-03-14 23:42:52 +0000

    Update Countries for PE06

--- PE06-CitiesWithCountriesCurrencyStackNav/src/Countries/Countries.js
+++ PE06-CitiesWithCountriesCurrencyStackNav/src/Countries/Countries.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { View, FlatList, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import CenterMessage from '../CenterMessage';
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, navigation, updateCurrency }) => {
   if (!countries.length) {
     return <CenterMessage message="No saved countries!" />;
   }
@@ -12,15 +12,23 @@ const Countries = ({ countries }) => {
     <View style={{ flex: 1 }}>
       <FlatList
         data={countries}
-        renderItem={({ item, index }) => (
-          <TouchableOpacity style={styles.item}>
-            <Text style={styles.name}>{item.name}</Text>
-            {!!item.currency && (
-              <Text style={styles.currency}>{item.currency}</Text>
-            )}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('Country', {
+                name: item.name,
+                currency: item.currency,
+                updateCurrency,
+              })
+            }
+          >
+            <View style={styles.countryContainer}>
+              <Text style={styles.countryName}>{item.name}</Text>
+              <Text style={styles.currencyText}>{item.currency}</Text>
+            </View>
           </TouchableOpacity>
         )}
-        keyExtractor={(item, index) => `${item.name}-${index}`}
       />
     </View>
   );
@@ -28,21 +36,24 @@ const Countries = ({ countries }) => {
 
 Countries.propTypes = {
   countries: PropTypes.array.isRequired,
+  navigation: PropTypes.object.isRequired,
+  updateCurrency: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
-  item: {
+  countryContainer: {
     padding: 20,
     borderBottomWidth: 1,
-    borderBottomColor: '#d4d4d4',
+    borderBottomColor: '#ccc',
   },
-  name: {
+  countryName: {
     fontSize: 22,
+    fontWeight: 'bold',
   },
-  currency: {
-    fontSize: 16,
+  currencyText: {
+    fontSize: 18,
     color: '#666',
-    marginTop: 4,
+    marginTop: 5,
   },
 });
 
